1101447480f69763b0e9c8b2e8f152b6529923aa
Randy <74595594+randy-ariesta-muliawan@users.noreply.github.com>
2025-02-15 19:29:52 +0700

    Update main.py

--- Weather Forecasting/main.py
+++ Weather Forecasting/main.py
@@ -1,8 +1,10 @@
-from js import XMLHttpRequest
+from js import XMLHttpRequest, document
 import json
 from time import gmtime, strftime
+from datetime import datetime, timedelta
 
-today_time = strftime("%a, %H:%M, %d %b", gmtime())
+now = datetime.utcnow() + timedelta(hours=7)
+today_time = now.strftime("%a, %H:%M, %d %b")
 document.getElementById("time").innerText = today_time
 
 
@@ -13,6 +15,11 @@ def make_request(url):
     output = str(req.response)
     return output
 
+def update_time():
+    now = datetime.utcnow() + timedelta(hours=7)  # Menambahkan 7 jam untuk WIB
+    current_time = now.strftime("%a, %H:%M, %d %b")  
+    document.getElementById("time").innerText = current_time
+
 def get_ip():
     API_key = 'c5353e5225f64d51895f9dde3389ca97'
     out_put = make_request(f'https://api.ipgeolocation.io/ipgeo?apiKey={API_key}')
@@ -21,10 +28,7 @@ def get_ip():
 
 
 def get_weather():
-    lat = get_ip()['latitude']
-    lon =  get_ip()['longitude']
-    API_key =  '47e28d589c912d35b8aed44a6681c3c2'
-    out_put = make_request(f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_key}')
+    out_put = make_request(f'https://api.openweathermap.org/data/2.5/weather?lat=-2.9170&lon=104.7285&appid=47e28d589c912d35b8aed44a6681c3c2')
     weather = json.loads(out_put)
     return weather
 
